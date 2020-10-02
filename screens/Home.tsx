ebb7ef5f4847b65f3c5a674315e6d0b4f3c5ca70
Nathan <nquinn@medonesystems.com>
2020-10-01 20:25:51 -0400

    wef

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,16 +1,10 @@
 import * as React from "react";
 import { StyleSheet } from "react-native";
 
-import { Text, View } from "../components/Themed";
+import { Text } from "../components/Themed";
 import { Main } from "../store/Store.mobx";
-import { ListItem, Tile, Image } from "react-native-elements";
+import { Tile } from "react-native-elements";
 import ScrollRefresh from "../components/ScrollRefresh";
-const images: any = {
-  Burgers: require("../assets/images/burger.jpg"),
-  Sandwitches: require("../assets/images/sandwitch.jpg"),
-  Appetizers: require("../assets/images/appetizer.jpg"),
-  Sides: require("../assets/images/side.jpg"),
-};
 
 export default function Home({ navigation }: any) {
   const store = React.useContext(Main);
@@ -32,24 +26,6 @@ export default function Home({ navigation }: any) {
       <ScrollRefresh onRefresh={() => setRefreshing(true)}>
         {data.map((v, i) => {
           return (
-            // <View
-            //   style={{
-            //     width: "100%",
-            //     height: 300,
-            //     alignItems: "center",
-            //     justifyContent: "center",
-            //   }}
-            //   key={i}
-            // >
-            //   <Text>{v.IMAGE}</Text>
-            //   <Image
-            //     source={{ uri: v.IMAGE }}
-            //     style={{
-            //       width: 300,
-            //       height: 300,
-            //     }}
-            //   />
-            // </View>
             <Tile
               key={i}
               imageSrc={{ uri: v.IMAGE }}
@@ -61,23 +37,12 @@ export default function Home({ navigation }: any) {
                 navigation.navigate("ItemList");
               }}
             />
-            // <ListItem
-            //   key={i}
-            //   title={v.name}
-            //   bottomDivider
-            //   chevron
-            //   onPress={() => {
-            //     categories.setCurrent(v);
-            //     navigation.navigate("ItemList");
-            //   }}
-            // />
           );
         })}
       </ScrollRefresh>
     </Main.Provider>
   );
 }
-// export default inject("store")(observer(ImageList));
 const styles = StyleSheet.create({
   container: {
     flex: 1,
